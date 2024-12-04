c64fd8fc8436e01bc945efd406f60e05000085fc
Barry <maladhobarry4928@gmail.com>
2024-12-04 13:14:43 +0100

    suppression fichier

--- projetcpp/Bouton.cpp
+++
@@ -1,9 +0,0 @@
-#include "Bouton.h"
-
-Bouton::Bouton(int pinBouton) : pin(pinBouton) {
-    pinMode(pin, INPUT_PULLUP);
-}
-
-bool Bouton::estAppuye() const {
-    return digitalRead(pin) == HIGH;
-}
